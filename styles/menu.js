6508ab635cebf07c173657ef3b0836e3459eb668
caramessina <messina.c@husky.neu.edu>
2019-07-28 14:16:03 -0400

    test

---
+++ styles/menu.js
@@ -0,0 +1,29 @@
+document.write('\
+\
+<link rel="stylesheet" href="/styles/menu.css"></link>\
+\
+\<ul class="topnav">\
+\
+<li class="logo">Cara Marta Messina </li>\
+\
+  <li><a href="/index.html">About Me</a></li>\
+  \  <li><a href="/about/Messina_CV.pdf">CV</a></li>\
+ \
+  <li class="dropdown">\
+   <a href="javascript:void(0)" class="dropbtn">Fan Studies</a>\
+  \<div class="dropdown-content">\
+  \<a href="/fan-studies/index.html">About Fan Studies</a>\
+  \<a href="/fan-studies/models/all_ff_topic_model.html">Topic Model: All Fanfics</a>\
+  \</div>\
+  \</li>\
+    \
+  <li><a href="/pedagogy/index.html">Pedagogy</a></li>\
+      \
+  <li><a href="/writing/index.html">Writing</a></li>\
+      \
+  <li><a href="https://github.com/caramessina">GitHub</a></li>\
+  \
+  <li><a href="/contact/index.html">Contact</a></li>\
+  \
+</ul>\
+')
