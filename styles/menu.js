362d4f3b524baa437b314753a4f75306240c0cdd
Cara Marta Messina <messina.c@husky.neu.edu>
2020-04-22 18:34:00 -0400

    fix broken

--- styles/menu.js
+++ styles/menu.js
@@ -12,7 +12,7 @@ document.write('\
 
  \
   <li class="dropdown">\
-   \<a href="javascript:void(0)" class="dropbtn">Fan Studies</a>\
+   <a href="javascript:void(0)" class="dropbtn">Fan Studies</a>\
   \<div class="dropdown-content">\
   \<a href="/fan-studies/index.html">About Fan Studies</a>\
   \<a href="/fan-studies/models/all_ff_topic_model.html">Topic Model: All Fanfics</a>\
@@ -20,7 +20,7 @@ document.write('\
   \</li>\
   \
   <li class="dropdown">\
-   \<a href="javascript:void(0)" class="dropbtn">Teaching</a>\
+   <a href="javascript:void(0)" class="dropbtn">Teaching</a>\
   \<div class="dropdown-content">\
   \<a href="/pedagogy/index.html">Digital Critical Pedagogy</a>\
   \<a href="/pedagogy/remiXML.html">remiXML: First Year Writing</a>\
